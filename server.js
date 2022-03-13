ccb098d599043e506ede7d6902c6800fb9bc3fd4
freescout <neethu.jacob@gmail.com>
2022-03-14 00:52:21 +0100

    Add console logs

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const router = require('./routes/index');
 const path = require('path')
+console.log(`Input server port ${process.env.PORT}`);
 const PORT = process.env.PORT || 3001;
 require('dotenv').config()
 const app = express();
@@ -11,7 +12,7 @@ const app = express();
 var corsOptions = {
   origin: ['https://scot-users.herokuapp.com', 'http://localhost:3000']
 };
-app.use(cors(corsOptions))
+//app.use(cors(corsOptions))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/api', router);
@@ -33,5 +34,5 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.listen(PORT, function() {
-  console.log(`Server listening on port ${PORT}.`);
+  console.log(`Server listening on port ${PORT}`);
 });
