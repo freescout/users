535feb67485ce4507c290a139859c8d4d1258c05
freescout <neethu.jacob@gmail.com>
2022-03-14 12:53:53 +0100

    Log server API port in client

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 const PORT = process.env.PORT || 3001;
 
+console.log(`Server API port ${PORT}`);
 export default axios.create({
   baseURL: `http://localhost:${PORT}/api`
 });
